3560efbe279a780508e08666a1ac309584c505b4
Gus Meyer <gus@robotics88.com>
2024-01-09 12:08:05 -0800

    Just commenting out the whole 'if' statement

--- FAST-LIO/src/IMU_Processing.hpp
+++ FAST-LIO/src/IMU_Processing.hpp
@@ -194,11 +194,12 @@ void ImuProcess::IMU_init(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 
   init_state.grav = S2(- mean_acc / mean_acc.norm() * G_m_s2);
   const auto& orientation = meas.imu.front()->orientation;
   // 如果orientation消息可用的话,以IMU的orientaion作为初始姿态
-  if( orientation.x != 0 || orientation.y != 0 || orientation.z != 0 || orientation.w != 0 ){
-    // Eigen::Quaterniond qua(orientation.w, orientation.x, orientation.y, orientation.z);
-    Eigen::Quaterniond qua(1.0 , 0.0, 0.0, 0.0);
-    init_state.rot = qua;
-  }
+  // if( orientation.x != 0 || orientation.y != 0 || orientation.z != 0 || orientation.w != 0 ){
+  //   Eigen::Quaterniond qua(orientation.w, orientation.x, orientation.y, orientation.z);
+  //   init_state.rot = qua;
+  // }
+
+
   //state_inout.rot = Eye3d; // Exp(mean_acc.cross(V3D(0, 0, -1 / scale_gravity)));
   init_state.bg  = mean_gyr;
   init_state.offset_T_L_I = Lidar_T_wrt_IMU;
